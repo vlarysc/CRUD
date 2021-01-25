ec24d472e15383a1bdca6b0ed816fda94350e59f
Vlary <manoelfranciscose@gmail.com>
2021-01-25 15:27:51 -0300

    CRUD

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,40 +1,45 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
+  <q-layout
+    view="hHh Lpr lff"
+    container
+    style="height: 650px"
+    class="shadow-2 rounded-borders"
+  >
+    <q-header elevated class="bg-black">
       <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="leftDrawerOpen = !leftDrawerOpen"
-        />
-
-        <q-toolbar-title>
-          Cadastro
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
+        <q-btn flat @click="drawer = !drawer" round dense icon="menu" />
+        <q-toolbar-title>Header</q-toolbar-title>
       </q-toolbar>
     </q-header>
 
     <q-drawer
-      v-model="leftDrawerOpen"
+      v-model="drawer"
       show-if-above
+      :width="200"
+      :breakpoint="500"
       bordered
-      content-class="bg-grey-1"
+      content-class="bg-grey-3"
     >
-      <q-list>
-        <q-item-label header class="text-grey-8">
-          Essential Links
-        </q-item-label>
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
+      <q-scroll-area class="fit">
+        <q-list>
+          <template v-for="(menuItem, index) in menuList">
+            <q-item
+              :key="index"
+              clickable
+              :active="menuItem.label === 'Outbox'"
+              v-ripple
+            >
+              <q-item-section avatar>
+                <q-icon :name="menuItem.icon" />
+              </q-item-section>
+              <q-item-section>
+                {{ menuItem.label }}
+              </q-item-section>
+            </q-item>
+            <q-separator :key="'sep' + index" v-if="menuItem.separator" />
+          </template>
+        </q-list>
+      </q-scroll-area>
     </q-drawer>
 
     <q-page-container>
@@ -44,63 +49,9 @@
 </template>
 
 <script lang="ts">
-import EssentialLink from 'components/EssentialLink.vue';
-
-const linksData = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-];
-
-import { defineComponent, ref } from '@vue/composition-api';
+import { defineComponent } from '@vue/composition-api';
 
 export default defineComponent({
-  name: 'MainLayout',
-  components: { EssentialLink },
-  setup() {
-    const leftDrawerOpen = ref(false);
-    const essentialLinks = ref(linksData);
-
-    return { leftDrawerOpen, essentialLinks };
-  }
+  name: 'MainLayout'
 });
 </script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,270 +1,140 @@
 <template>
-  <q-page class="container">
-    <q-page class="">
-      <div class="row">
-        <q-form @submit="cadastrado" @reset="resetar" class=""> </q-form>
-      </div>
-      <div class="q-pa-md">
-        <div class="row">
-          <div class="col-4">
-            <q-input filled v-model="form.name" label="Nome" />
-          </div>
-          <div class="col-4">
-            <q-input filled v-model="form.nickName" label="Sobrenome" />
-          </div>
-          <div class="col-4">
-            <q-input
-              class="e-mail"
-              filled
-              v-model="form.email"
-              type="email"
-              label="Email"
-            />
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-4">
-            <q-input filled v-model="form.documento1" label="CPF ou CNPJ" />
-          </div>
-          <div class="col-4">
-            <q-input filled v-model="form.documento2" label="RG ou IE" />
-          </div>
-          <div class="col-4">
-            <q-input filled v-model="form.documento3" label="IM" />
-          </div>
-        </div>
+  <div class="q-pa-md">
+    <q-btn
+      :loading="loading3"
+      :percentage="percentage3"
+      dark-percentage
+      unelevated
+      color="orange"
+      text-color="grey-9"
+      @click="startComputing(3)"
+      icon="cloud_upload"
+      style="width: 100px; margin: 10px;"
+    />
+    <q-table title="Treats" :data="data" :columns="columns" row-key="name">
+      <template v-slot:header-selection="scope">
+        <q-toggle v-model="scope.selected" />
+      </template>
 
-        <div class="row">
-          <div class="col-4">
-            <q-input
-              v-model.number="form.telefone1"
-              filled
-              type="tel"
-              label="Telefone 1"
-              :rules="[myRule]"
+      <template v-slot:body-cell-option="props">
+        <q-td :props="props">
+          <div class="q-pa-md q-gutter-sm">
+            <q-btn
+              round
+              color="yellow-14"
+              @click="clicou(props.row)"
+              icon="create"
+            />
+            <q-btn
+              round
+              color="red"
+              @click="clicou(props.row)"
+              text-color="white"
+              icon="delete_forever"
             />
           </div>
-          <div class="col-4">
-            <q-input
-              v-model.number="form.telefone2"
-              filled
-              type="tel"
-              label="Telefone 2"
-            />
-          </div>
-          <div class="col-4">
-            <q-input
-              v-model.number="form.telefone3"
-              filled
-              type="tel"
-              label="Telefone 3"
-            />
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-4">
-            <q-input
-              v-model="form.cep"
-              @blur="buscaCep"
-              filled
-              type="tel"
-              label="CEP"
-            />
-          </div>
-          <div class="col-4">
-            <q-input
-              v-model.number="form.cidade"
-              filled
-              type="tel"
-              label="Cidade"
-            />
-          </div>
-          <div class="col-4">
-            <q-input
-              v-model.number="form.bairro"
-              filled
-              type="tel"
-              label="Bairro"
-            />
-          </div>
-          <div class="col-4">
-            <q-input
-              v-model.number="form.logradouro"
-              filled
-              type="tel"
-              label="Rua"
-            />
-          </div>
-          <div class="col-4">
-            <q-input
-              v-model.number="form.complemento"
-              filled
-              type="tel"
-              label="Complemento"
-            />
-          </div>
-          <div class="col-4">
-            <q-input v-model="form.uf" filled type="tel" label="UF" />
-            <select name="uf" v-model="form.uf">
-              <option value="AC">Acre</option>
-              <option value="AL">Alagoas</option>
-              <option value="AP">Amapá</option>
-              <option value="AM">Amazonas</option>
-              <option value="BA">Bahia</option>
-              <option value="CE">Ceará</option>
-              <option value="DF">Distrito Federal</option>
-              <option value="ES">Espírito Santo</option>
-              <option value="GO">Goiás</option>
-              <option value="MA">Maranhão</option>
-              <option value="MT">Mato Grosso</option>
-              <option value="MS">Mato Grosso do Sul</option>
-              <option value="MG">Minas Gerais</option>
-              <option value="PA">Pará</option>
-              <option value="PB">Paraíba</option>
-              <option value="PR">Paraná</option>
-              <option value="PE">Pernambuco</option>
-              <option value="PI">Piauí</option>
-              <option value="RJ">Rio de Janeiro</option>
-              <option value="RN">Rio Grande do Norte</option>
-              <option value="RS">Rio Grande do Sul</option>
-              <option value="RO">Rondônia</option>
-              <option value="RR">Roraima</option>
-              <option value="SC">Santa Catarina</option>
-              <option value="SP">São Paulo</option>
-              <option value="SE">Sergipe</option>
-              <option value="TO">Tocantins</option>
-            </select>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <q-btn
-          color="amber"
-          type="reset"
-          glossy
-          label="Cadastrar"
-          class="float-right"
-        />
-      </div>
-    </q-page>
-
-    <router-view />
-  </q-page>
+        </q-td>
+      </template>
+    </q-table>
+  </div>
 </template>
 
 <script>
-import axios from 'axios';
-
 export default {
   name: 'PageIndex',
-  props: {
-    pessoa: {
-      nome: 'string',
-      nickName: 'string',
-      email: 'string', //com validacao de formato de email
-      tipoPessoa: 'string', //(PF,PJ)
-      documento1: 'string', //campo unico
-      documento2: 'string',
-      documento3: 'string',
-      telefone1: 'string', // campo obrigatorio
-      telefone2: 'string',
-      telefone3: 'string',
-      enderecos: [
-        {
-          cep: 'string',
-          logradouro: 'string',
-          complemento: 'string',
-          bairro: 'string',
-          cidade: 'string',
-          uf: 'string'
-        }
-      ]
+  methods: {
+    clicou(props) {
+      this.$q.notify({
+        type: 'positive',
+        message: `Clicou em ${props.name}`
+      });
     }
   },
   data() {
     return {
-      leftDrawerOpen: false,
-      cadastrado: false,
-      form: {
-        documento1: '',
-        documento2: '',
-        documento3: '',
-        telefone1: '',
-        telefone2: '',
-        telefone3: '',
-        email: '',
-        date: '',
-        text: '',
-        model: '',
-        name: '',
-        nickName: '',
-        enderecos: []
-      }
+      columns: [
+        {
+          name: 'tipoPessoa',
+          label: 'Tipo',
+          align: 'left',
+          field: 'tipoPessoa',
+          sortable: true
+        },
+        {
+          name: 'nome',
+          align: 'left',
+          label: 'Nome',
+          field: 'name',
+          sortable: true
+        },
+        {
+          name: 'nickName',
+          align: 'left',
+          label: 'Sobrenome',
+          field: 'nickName',
+          sortable: true
+        },
+        { name: 'email', align: 'center', label: 'E-mail', field: 'email' },
+        {
+          name: 'telefone1',
+          align: 'center',
+          label: 'Telefone(1)',
+          field: 'telefone1'
+        },
+        {
+          name: 'documento1',
+          align: 'center',
+          label: 'CPF ou CNPJ',
+          field: 'documento1'
+        },
+        {
+          name: 'nascimento',
+          align: 'center',
+          label: 'Data de Nascimento',
+          field: 'nascimento',
+          sortable: true
+        },
+
+        { name: 'option', align: 'center', label: 'Opções', sortable: false }
+      ],
+      data: [
+        {
+          tipoPessoa: 'PF',
+          name: 'Manoel',
+          nickName: 'Neto',
+          email: 'manoelneto@gmail.com',
+          documento1: 28009246588,
+          telefone1: 54996901309,
+          nascimento: 11071994
+        }
+      ],
+      loading1: false,
+      percentage1: 0,
+
+      loading2: false,
+      percentage2: 0,
+
+      loading3: false,
+      percentage3: 0
     };
   },
-
   methods: {
-    buscaCep() {
-      resultado = axios
-        .get(`http://viacep.com.br/ws/${this.form.cep}/json/`)
-        .then(res => res)
-        .catch(error => error);
-      this.resultado.data.ce = cep;
-
-      this.resultado.data.longadouro = longadouro;
-      this.resultado.data.complemento = complemento;
-      this.resultado.data.bairro = bairro;
-      this.resultado.data.cidade = cidade;
-      this.resultado.data.uf = uf;
-
-      this.form.enderecos.push(this.enderecos);
-    },
-    myRule(val) {
-      return new Promise((resolve, reject) => {
-        setTimeout(() => {
-          resolve(!!val || '* Campo Obrigatório');
-        }, 500);
-      });
-    },
-
-    reset() {
-      this.$refs.input.resetValidation();
-    },
-    resetar() {
-      (this.form = {
-        documento1: '',
-        documento2: '',
-        documento3: '',
-        telefone1: '',
-        telefone2: '',
-        telefone3: '',
-        password: '',
-        email: '',
-        name: '',
-        nickName: '',
-        cep: '',
-        logradouro: '',
-        complemento: '',
-        bairro: '',
-        cidade: '',
-        uf: ''
-      }),
-        this.$q.notify({
-          color: 'green-4',
-          textColor: 'white',
-          icon: 'cloud_done',
-          message: 'Submitted'
-        });
+    startComputing(id) {
+      this[`loading${id}`] = true;
+      this[`percentage${id}`] = 0;
+      this[`interval${id}`] = setInterval(() => {
+        this[`percentage${id}`] += Math.floor(Math.random() * 8 + 10);
+        if (this[`percentage${id}`] >= 100) {
+          clearInterval(this[`interval${id}`]);
+          this[`loading${id}`] = false;
+        }
+      }, 700);
     }
+  },
+
+  beforeDestroy() {
+    clearInterval(this.interval1);
+    clearInterval(this.interval2);
   }
 };
 </script>
-
-<style lang="sass" scoped>
-.row > div
-  padding: 10px 15px
-  background: rgba(86,61,124,.15)
-  border: 1px solid rgba(86,61,124,.2)
-.row + .row
-  margin-top: 1rem
-</style>
